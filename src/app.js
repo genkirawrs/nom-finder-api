93fc4d11dc6e8c52cfee9c11d21530300d1b25cd
Rosalind H <rosalind.hung99@gmail.com>
2020-07-09 01:41:59 -0700

    adding cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 
+const {CLIENT_ORIGIN} = require('./config');
+
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -14,6 +16,12 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
 
 app.get('/', (req, res) => {
    res.send('Hello, boilerplate!')
